b1eb166ceebbd9c05d879341906678a5532e4b40
JohnathanMB <johnathan.monsalve@udea.edu.co>
2024-01-20 11:31:53 -0500

    initial configuration

---
+++ src/main/java/com/johnathan/techinaltestbackend/TechinalTestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.johnathan.techinaltestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechinalTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechinalTestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnathan/techinaltestbackend/TechinalTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnathan.techinaltestbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechinalTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
