406a4ba85bdb024b3bac9a6f6b0b9990ca80b708
JohnathanMB <johnathan.monsalve@udea.edu.co>
2024-01-20 11:50:22 -0500

    Create module core and Record Client

---
+++ core/src/main/java/org/example/model/Client.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+public record Client(
+        String idType,
+        String idNumber,
+        String firstName,
+        String secondName,
+        String firstLastname,
+        String secondLastName,
+        String phoneNumber,
+        String address,
+        String city
+) {
+}
