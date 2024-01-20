4cd9a735ebc2b650f8ec179648c462fba4bd9285
JohnathanMB <johnathan.monsalve@udea.edu.co>
2024-01-20 15:06:44 -0500

    refactor responde methods

--- adapter/entry-point/src/main/java/org/example/ClientController.java
+++ adapter/entry-point/src/main/java/org/example/ClientController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @org.springframework.web.bind.annotation.RestController
 public class ClientController {
 
@@ -19,19 +21,31 @@ public class ClientController {
 
     @GetMapping("/hi")
     private ResponseEntity<String> testApi(){
-        return new ResponseEntity<String>("HOLA", HttpStatus.ACCEPTED);
+        return new ResponseEntity<>("HOLA", HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/client/{idType}/{idNumber}")
+    @GetMapping("/clients/")
+    private ResponseEntity<ResponseDto> getClients(){
+
+        List<Client> clients = clientService.getClients();
+        return validateContent(!clients.isEmpty(), clients);
+
+    }
+
+    @GetMapping("/clients/{idType}/{idNumber}")
     private ResponseEntity<ResponseDto> getClient(@PathVariable("idType") String idType,
                                                   @PathVariable("idNumber") String idNumber){
 
         Client client = clientService.getClient(idType.toUpperCase(), idNumber);
 
-        return !client.idType().isEmpty()
-                ? ResponseEntity.ok().body(new ResponseDto<>(HttpStatus.OK.value(), client))
-                : ResponseEntity.badRequest().body(new ResponseDto<>(HttpStatus.NOT_FOUND.value(), "Client No found"));
+        return validateContent(!client.idType().isEmpty(), client);
 
+    }
+
+    private static ResponseEntity<ResponseDto> validateContent(boolean validation, Object body) {
+        return validation
+                ? ResponseEntity.ok().body(new ResponseDto<>(HttpStatus.OK.value(), body))
+                : ResponseEntity.badRequest().body(new ResponseDto<>(HttpStatus.NOT_FOUND.value(), "Client Not found"));
     }
 
 }
