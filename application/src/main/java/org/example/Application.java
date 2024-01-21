9cc6ca42edea29bb1298bae027d3712bee32d7d4
JohnathanMB <johnathan.monsalve@udea.edu.co>
2024-01-21 00:17:29 -0500

    prueba de action

--- application/src/main/java/org/example/Application.java
+++ application/src/main/java/org/example/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("TODO: Eliminar luego, esto es una prueba");
         SpringApplication.run(Application.class, args);
     }
 }
